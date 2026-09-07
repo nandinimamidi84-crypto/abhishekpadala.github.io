1cdc736a9a2c28b74cb56ed792d3aca66105052f
nandinimamidi84-crypto <nandinimamidi84@gmail.com>
2026-09-07 13:21:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+
+    const nav = document.querySelector(".nav-links");
+
+    nav.classList.toggle("active");
+
+}
+
+function showContact() {
+    document.getElementById("contactPopup").style.display = "flex";
+}
+
+function closeContact() {
+    document.getElementById("contactPopup").style.display = "none";
+}
